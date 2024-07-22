18adbe29615de4367e3c2fc7f8aeb317d07714a7
Matthew Connelly <matthew@harbourshare.com>
2024-07-22 15:39:42 -0400

    toolbar: undo redo buttons

--- packages/super-editor/src/components/toolbar/items/historyRedo.js
+++ packages/super-editor/src/components/toolbar/items/historyRedo.js
@@ -4,13 +4,11 @@ const button = ToolbarItem({
     type: 'button',
     name: 'redo',
     tooltip: "Redo",
-    command: "redoAction",
+    command: "redo",
     icon: 'fa fa-rotate-right',
     preCommand(self) {
         clearTimeout(self.tooltipTimeout);
         self.tooltipVisible = false;
-        self.active = self.active ? false : true;
-        self.childItem.active = self.childItem.active ? false : true;
     },
     onMarkSelection(self, mark) {
     },
--- packages/super-editor/src/components/toolbar/items/historyUndo.js
+++ packages/super-editor/src/components/toolbar/items/historyUndo.js
@@ -4,13 +4,11 @@ const button = ToolbarItem({
     type: 'button',
     name: 'undo',
     tooltip: "Undo",
-    command: "undoAction",
+    command: "undo",
     icon: "fa-solid fa-rotate-left",
     preCommand(self) {
         clearTimeout(self.tooltipTimeout);
         self.tooltipVisible = false;
-        self.active = self.active ? false : true;
-        self.childItem.active = self.childItem.active ? false : true;
     },
     onMarkSelection(self, mark) {
     },
