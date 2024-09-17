c89f5babc611ab2432b4ba5246aae901108ebd3f
Artem Nistuley <101666502+artem-harbour@users.noreply.github.com>
2024-09-17 18:35:33 +0300

    sync ordered lists (#91)

--- packages/super-editor/src/extensions/ordered-list/ordered-list.js
+++ packages/super-editor/src/extensions/ordered-list/ordered-list.js
@@ -1,6 +1,7 @@
 import { Node, Attribute } from '@core/index.js';
 import { toKebabCase } from '@harbour-enterprises/common';
-import { generateDocxListAttributes, findParentNode } from '@helpers/index.js';
+import { generateDocxListAttributes, findParentNode, findChildren } from '@helpers/index.js';
+import { Plugin, PluginKey } from 'prosemirror-state';
 
 export const OrderedList = Node.create({
   name: 'orderedList',
@@ -34,6 +35,18 @@ export const OrderedList = Node.create({
             start: attrs.order,
           };
         },
+      },
+
+      syncId: {
+        default: null,
+        parseDOM: (elem) => elem.getAttribute('data-sync-id'),
+        renderDOM: (attrs) => {
+          if (!attrs.syncId) return {};
+          return {
+            'data-sync-id': attrs.syncId,
+          };
+        },
+        // rendered: false,
       },
 
       'list-style-type': {
@@ -99,7 +112,8 @@ export const OrderedList = Node.create({
         }
 
         if (dispatch) {
-          let listLevel = (list.depth - 1) / 2; // each list level increases depth by 2
+          // Each list level increases depth by 2.
+          let listLevel = (list.depth - 1) / 2;
           let listStyleTypes = this.options.listStyleTypes;
           let listStyle = listStyleTypes[listLevel % listStyleTypes.length];
           let currentListStyle = list.node.attrs['list-style-type'];
@@ -130,7 +144,7 @@ export const OrderedList = Node.create({
        *  <li>item</li>
        * </ol>
        */
-      continueListNumberingAfterLiftEmpty: () => ({
+      continueListOrderAfterLiftEmptyBlock: () => ({
         editor,
         dispatch,
         state,
@@ -144,21 +158,31 @@ export const OrderedList = Node.create({
         }
 
         let canLiftEmptyBlock = editor.can().liftEmptyBlock();
-        let isRootDepth = list.depth === 1;
-        if (!canLiftEmptyBlock || !isRootDepth) return false;
+        let isRootDepth = list.depth === 1; // Only first level lists.
+        let isOneItem = list.node.childCount === 1;
+        let currentListItem = state.selection.$from.node(-1);
+        let isLastItem = list.node.lastChild.eq(currentListItem);
+
+        let canRunCommand = canLiftEmptyBlock 
+          && isRootDepth 
+          && !isOneItem 
+          && !isLastItem;
+
+        if (!canRunCommand) {
+          return false;
+        }
 
         if (dispatch) {
-          let { $from } = state.selection;
-          // save pos before liftEmptyBlock command
-          let prevListPos = $from.before(-2);
+          // Save pos before liftEmptyBlock command.
+          let prevListPos = list.pos;
 
           if (!commands.liftEmptyBlock(state, dispatch)) {
             return false;
           }
 
-          let { $to } = state.selection;
+          let { $from } = tr.selection;
           let prevListNode = tr.doc.nodeAt(prevListPos);
-          let newListPos = $to.after();
+          let newListPos = $from.after();
           let newListNode = tr.doc.nodeAt(newListPos);
 
           let isPrevOrderedList = prevListNode?.type.name === this.name;
@@ -167,12 +191,22 @@ export const OrderedList = Node.create({
           if (isPrevOrderedList && isNewOrderedList) {
             let lastOrder = prevListNode.attrs.order || 1;
             let lastIndex = prevListNode.childCount;
-            let start = lastIndex + lastOrder;
+            let lastSyncId = prevListNode.attrs.syncId;
+            let newOrder = lastIndex + lastOrder;
+            let syncId = !!lastSyncId ? lastSyncId : randomId();
 
             tr.setNodeMarkup(newListPos, undefined, {
               ...newListNode.attrs,
-              order: start,
+              order: newOrder,
+              syncId,
             });
+
+            if (!lastSyncId) {
+              tr.setNodeMarkup(prevListPos, undefined, {
+                ...prevListNode.attrs,
+                syncId,
+              });
+            }
           }
         }
 
@@ -186,8 +220,85 @@ export const OrderedList = Node.create({
       'Mod-Shift-7': () => {
         return this.editor.commands.toggleOrderedList();
       },
+      Enter: () => {
+        return this.editor.commands.continueListOrderAfterLiftEmptyBlock();
+      },
     };
+  },
+
+  addPmPlugins() {
+    return [
+      new Plugin({
+        key: new PluginKey('orderedListSync'),
+        appendTransaction: (transactions, oldState, newState) => {
+          let docChanges = transactions.some((tr) => tr.docChanged) 
+            && !oldState.doc.eq(newState.doc);
+
+          if (!docChanges) return;
+
+          let { doc, tr } = newState;
+          let listsBySyncId = {};
+
+          doc.descendants((node, pos) => {
+            if (node.type.name === this.name && !!node.attrs.syncId) {
+              let syncId = node.attrs.syncId;
+              if (!listsBySyncId[syncId]) listsBySyncId[syncId] = [];
+              listsBySyncId[syncId].push({ node, pos });
+            }
+          });
+
+          let hasListsToSync = !!Object.keys(listsBySyncId).length;
+
+          if (!hasListsToSync) {
+            return;
+          }
+
+          let changed = false;
+          Object.entries(listsBySyncId).forEach(([_syncId, lists]) => {
+            // If there are less than 2 lists, then we have nothing to sync.
+            if (lists.length < 2) {
+              let [firstList] = lists;
+              tr.setNodeMarkup(firstList.pos, undefined, {
+                ...firstList.node.attrs,
+                syncId: null,
+              });
+
+              changed = true;
+              return;
+            }
+
+            let [firstList] = lists;
+            let currentOrder = firstList.node.attrs.order;
+
+            lists.forEach((list, index) => {
+              // Skip the first list.
+              if (index === 0) return;
+
+              let { node, pos } = list;
+              let prevList = lists[index - 1];
+              let newOrder = currentOrder + prevList.node.childCount;
+
+              if (node.attrs.order !== newOrder) {
+                tr.setNodeMarkup(pos, undefined, {
+                  ...node.attrs,
+                  order: newOrder,
+                });
+                changed = true;
+              }
+
+              currentOrder = newOrder;
+            });
+          });
+
+          return changed ? tr : null;
+        },
+      }),
+    ]
   },
 
   // Input rules.
 });
+
+function randomId() {
+  return Math.floor(Math.random() * 0xffffffff).toString();
+};
