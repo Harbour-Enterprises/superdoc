cfdbc7e54d3ed04609063a8bfe0d4ea3acca1182
Nick Bernal <nick@harbourshare.com>
2025-01-21 14:44:22 -0800

    revert

--- packages/super-editor/src/core/commands/sinkListItem.js
+++ packages/super-editor/src/core/commands/sinkListItem.js
@@ -1,7 +1,5 @@
 import { sinkListItem as originalSinkListItem } from 'prosemirror-schema-list';
 import { getNodeType } from '../helpers/getNodeType.js';
-import { findParentNode } from '@helpers/index.js';
-import { TextSelection } from "prosemirror-state";
 
 /**
  * Sink list item down into an inner list.
@@ -10,11 +8,7 @@ import { TextSelection } from "prosemirror-state";
  * https://prosemirror.net/docs/ref/#schema-list.sinkListItem
  */
 //prettier-ignore
-export const sinkListItem = (typeOrName) => ({ state, dispatch, tr }) => {
+export const sinkListItem = (typeOrName) => ({ state, dispatch }) => {
   const type = getNodeType(typeOrName, state.schema);
-
-  const list = findParentNode((node) => node.type.name === 'orderedList')(state.selection);
-  tr.setMeta('originalList', list);
-
   return originalSinkListItem(type)(state, dispatch);
 };
